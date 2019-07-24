a43561cc9b2361a256e57343a475adf886b40447
vladikturin <pro100alex1999@yandex.ru>
2019-07-25 00:30:58 +0300

    Some regularization was added

--- main.py
+++ main.py
@@ -2,35 +2,36 @@ from tools import *
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv("/Users/alextyurin/Desktop/pycharm_projects/recognition/train.csv")
+data = pd.read_csv("/Users/vladikturin/Desktop/pycharm_projects/recognition/train.csv")
 Y = np.array(data.label).reshape([len(data.label), 1])
 Y = np.array([np.eye(10)[el[0], :] for el in Y]).T
 
 X = np.array(data.iloc[:, 1:]).T
 X = X - np.mean(X) / np.std(X)
 # print(X.shape)
-#
+
 # parameters = initialize_parameters((784, 500, 250, 100, 50, 10))
 #
-# AL, caches = L_model_forward(X, parameters)
+# AL, caches = L_model_forward(X, parameters, "dropout", 0.5)
 #
 # for key, value in caches.items():
 #     print(f"{key}: shape of A_prev: {value[0].shape}, shape of Z: {value[1].shape}, shape of W: {value[2].shape}, shape of b: {value[3].shape}")
-#
-# grads = L_model_backward(AL, Y, caches)
-# print(grads)
+
 
 layer_dims = (784, 25, 10)
 learning_rate = 0.001
-num_iterations = 500
+num_iterations = 50
 
-parameters = L_layer_model(X, Y, layer_dims, learning_rate, num_iterations)
+parameters1 = L_layer_model(X, Y, layer_dims, learning_rate, num_iterations, "he")
+parameters2 = L_layer_model(X, Y, layer_dims, learning_rate, num_iterations, "normal")
 
-AL, caches = L_model_forward(X, parameters)
+AL1, caches1 = L_model_forward(X, parameters1)
+AL2, caches2 = L_model_forward(X, parameters2)
 
-accuracy = results(AL, Y)
+accuracy1 = results(AL1, Y)
+accuracy2 = results(AL2, Y)
 
-print(accuracy)
+print(accuracy1, accuracy2)
 # X = pd.DataFrame(X)
 # X[X > 0] = 1
 # X[X <= 0] = 0
--- tools.py
+++ tools.py
@@ -16,7 +16,7 @@ def tanh(Z):
     return np.tanh(Z)
 
 
-def initialize_parameters(layer_dims, initialization_parameter = "he"):
+def initialize_parameters(layer_dims, initialization_parameter="he"):
     parameters = {}
     L = len(layer_dims)
 
@@ -27,48 +27,73 @@ def initialize_parameters(layer_dims, initialization_parameter = "he"):
 
     elif initialization_parameter == "he":
         for l in range(1, L):
-            parameters["W" + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]) * np.sqrt(2/layer_dims[l-1])
+            parameters["W" + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]) * np.sqrt(2/layer_dims[l-1]) * 0.1
             parameters["b" + str(l)] = np.zeros((layer_dims[l], 1))
-
 
     return parameters
 
 
-def linear_activation_forward(A_prev, W, b, activation):
+def linear_activation_forward(A_prev, W, b, activation, keep_prop):
+    Z = np.dot(W, A_prev) + b
+    D = np.random.rand(Z.shape[0], Z.shape[1])
+    D = D >= keep_prop
+
     if activation == "sigmoid":
-        Z = np.dot(W, A_prev) + b
         A = sigmoid(Z)
-
     elif activation == "relu":
-        Z = np.dot(W, A_prev) + b
         A = relu(Z)
+    elif activation == "tanh":
+        A = tanh(Z)
 
+    A = A * D
     cache = (A_prev, Z, W, b)
     return A, cache
 
 
-def L_model_forward(X, parameters):
+def L_model_forward(X, parameters, regular_type, keep_prop):
     caches = {}
     A = X
     L = len(parameters) // 2
-    for l in range(1, L):
-        A_prev = A
-        W = parameters["W" + str(l)]
-        b = parameters["b" + str(l)]
-        A, cache = linear_activation_forward(A_prev, W, b, "relu")
-        caches[str(l)] = cache
+    if regular_type == "none" or regular_type == "l2":
+        for l in range(1, L):
+            A_prev = A
+            W = parameters["W" + str(l)]
+            b = parameters["b" + str(l)]
+            A, cache = linear_activation_forward(A_prev, W, b, "relu", 0)
+            caches[str(l)] = cache
 
-    W = parameters["W" + str(L)]
-    b = parameters["b" + str(L)]
-    AL, cache = linear_activation_forward(A, W, b, "sigmoid")
-    caches[str(L)] = cache
+        W = parameters["W" + str(L)]
+        b = parameters["b" + str(L)]
+        AL, cache = linear_activation_forward(A, W, b, "sigmoid", 0)
+        caches[str(L)] = cache
+
+    elif regular_type == "dropout":
+        for l in range(1, L):
+            A_prev = A
+            W = parameters["W" + str(l)]
+            b = parameters["b" + str(l)]
+            A, cache = linear_activation_forward(A_prev, W, b, "relu", keep_prop)
+            caches[str(l)] = cache
+
+        W = parameters["W" + str(L)]
+        b = parameters["b" + str(L)]
+        AL, cache = linear_activation_forward(A, W, b, "sigmoid", 0)
+        caches[str(L)] = cache
 
     return AL, caches
 
 
-def compute_cost(AL, Y):
+def compute_cost(AL, Y, regular_type, parameters):
     m = Y.shape[1]
     cost = -(Y * np.log(AL) + (1 - Y) * np.log(1 - AL)).sum() / m
+
+    l2_cost = 0
+    L = len(parameters) // 2
+
+    for i in range(1, L):
+        l2_cost += np.sum(parameters["W" + str(i)])
+
+    cost += l2_cost
 
     return cost
 
@@ -150,15 +175,15 @@ def results(AL, Y):
     print(np.array((al == Y), dtype=float).mean())
 
 
-def L_layer_model(X, Y, layer_dims, learning_rate, num_iterations, initialization_parameter):
+def L_layer_model(X, Y, layer_dims, learning_rate, num_iterations, initialization_parameter, regular_type="none", keep_prop=0):
     costs = []
 
     parameters = initialize_parameters(layer_dims, initialization_parameter)
 
     for i in range(num_iterations):
-        AL, caches = L_model_forward(X, parameters)
+        AL, caches = L_model_forward(X, parameters, regular_type, keep_prop)
 
-        cost = compute_cost(AL, Y)
+        cost = compute_cost(AL, Y, regular_type, parameters)
 
         grads = L_model_backward(AL, Y, caches)
 
